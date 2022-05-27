aa63af2ee9ed71e0e643692a9b90e30fc67c02db
andrew <andvila@hotmail.com>
2022-05-27 20:05:36 -0300

    Primer commit

---
+++ js/script1.js
@@ -0,0 +1,10 @@
+debugger
+
+let numero = parseInt(prompt("Ingrese un número menor a 100."))
+
+for(let i=1; i <= numero; i++){
+    console.log("¡HOLA!");
+    if(i==100){
+        break;
+    }
+}
